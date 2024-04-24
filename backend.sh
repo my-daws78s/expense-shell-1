ca8d818906cc58862b486e0caaa3cb3467b9f56d
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-24 16:42:09 +0530

    testing expense-shell-1 scripts using set -e and trap commands

--- backend.sh
+++ backend.sh
@@ -40,7 +40,7 @@ unzip /tmp/backend.zip &>>$LOGFILE
 
 npm install &>>$LOGFILE
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE 
+cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE 
 
 systemctl daemon-reload &>>$LOGFILE
 
