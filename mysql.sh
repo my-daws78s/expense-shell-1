355c1b473f8d17d4440688ddd6a1856ce42fbfd9
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-24 16:26:24 +0530

    testing expense-shell-1 scripts using set -e and trap commands

--- mysql.sh
+++ mysql.sh
@@ -10,32 +10,14 @@ echo "mysql password is: $mysql_secure_password"
 echo -e "$B Script Start time: $TIMESTAMP $N"
 
 ###############################
-dnf list installed mysql-server &>>$LOGFILE
-if [ $? -eq 0 ]
-then
-    echo -e "MYSQL is already installed.... $Y SKIPPING $N"
-else 
-    dnf install mysql-server -y &>>$LOGFILE
-    #VALIDATE $? "Installation of mysql"
-fi
+dnf install mysql-server -y &>>$LOGFILE
+systemctl enable mysqlds &>>$LOGFILE
+systemctl start mysqldf &>>$LOGFILE
 
-systemctl enable mysqld &>>$LOGFILE
-#VALIDATE $? "Enabling mysqld"
-
-systemctl start mysqld &>>$LOGFILE
-#VALIDATE $? "Starting of mysql"
-
-#Linux scripting is not idempotent by nature, user needs to take care:
-#Nature of program irrespective of how many times u run, it shud not change result.
-# mysql_secure_installation --set-root-pass $mysql_secure_password &>>$LOGFILE
-# VALIDATE $? "Setting up DB password"
-
-#mysql -h localhost -uroot -p${mysql_secure_password} -e 'show databases;' &>>$LOGFILE
 mysqll -h db.mydevops-learning.cloud -uroot -p${mysql_secure_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_secure_password} &>>$LOGFILE
-    #VALIDATE $? "Setting up DB password: "
 else 
     echo -e "mysql password is already setup. $Y SKIPPING $N"
 fi
